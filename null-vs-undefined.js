04e0a5fb3a8bf2fcd764c92fd17383d48c0c0baa
Sayem Parvez <sayemparvez.spf@gmail.com>
2021-02-02 12:35:01 +0600

    null-vs-undefined clear for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,31 @@
+// let pakhi;
+// console.log(pakhi);
+
+// function add(num1, num2){
+//     console.log(num1 + num2);
+// }
+// let num = add(12, 13);
+// console.log(num);
+
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return
+// }
+// let num = add(12, 13);
+// console.log(num);
+
+// function add(num1, num2){
+//     console.log(num2);
+//     return
+// }
+// let num = add(12);
+// console.log(num);
+
+// const premik = {name: "Smart Dude", phone: 02939374}
+// console.log(premik.gf);
+
+// let fun = undefined;
+// console.log(fun);
+
+let ages = [2, 3, 6];
+console.log(ages[11]);
